3f13b358bfc91d6b08538b29180d7ec547c855c6
Shuhei Terahata <shuhei.terahata@placeholder.co.jp>
2018-05-14 14:55:20 +0900

    ChaperoneUtilをstaticクラスに キャリブレーションのサンプルコード追加

---
+++ Assets/Litpla/Scripts/TestCalibration.cs
@@ -0,0 +1,14 @@
+﻿using Litpla.VR.Util;
+using UnityEngine;
+
+public class TestCalibration : MonoBehaviour
+{
+    [SerializeField] private KeyCode _key;
+    [SerializeField] private SteamVR_TrackedObject _trackedObj;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_key))
+            SteamVR_ChaperoneUtil.SetWorkingStandingZeroPoseFrom(_trackedObj);
+    }
+}
